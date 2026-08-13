328e152c9a9d2a06b7313dc9a78f684cbba4cc8a
BURHANUDDIN GANDHI <burhanuddingandhi7@gmail.com>
2026-08-13 15:27:27 +0530

    Fix relative User model import in auth routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../backend/models/Users.js";   
+import User from "../models/Users.js";   
 
 const router = express.Router();
 
